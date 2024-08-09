20b99eed1de7fc2ea6e60dec6bc4662c1ea71c68
Jonattan Sousa <jonattan.sousa@gmail.com>
2024-08-09 14:03:18 -0300

    Removi o código relacionado a veículo para focarmos em uma estrutura mais simples. Nesse ponto apenas mudei a herança de Decodable para Encodable (poderia ser Codable, que é composto por ambos), por meio do encodeImovel encodei um objeto para Data e depois passamos para String afim de exibir o JSON.

--- ParseJsonObjects.playground/Contents.swift
+++ ParseJsonObjects.playground/Contents.swift
@@ -1,201 +1,39 @@
 import Foundation
 
-// MARK: - Modelos de dados
-struct Vehicle: Decodable {
-    let nick: String
-    let model: String
-    let manufacturer: String
-    let plate: String
-    let productionYear: String
-    let acquisitionYear: String
-    let km: String
-    let services: [Maintenance]
-    let optionals: Optionals
-    
-    enum CodingKeys: String, CodingKey {
-        case services = "maintenanceHistory"
-        case nick, model, manufacturer, plate, productionYear, acquisitionYear, km, optionals
-    }
-    
-    struct Maintenance: Decodable {
-        let type: MaintenanceType
-        let resume: String
-        let km: String
-        let price: String
-        
-        enum MaintenanceType: Int, Decodable {
-            case oil, brake, tire, suspension, eletric
-            
-            var title: String {
-                switch self {
-                case .oil:
-                    return "Oil 🛢️"
-                case .brake:
-                    return "Brakes 🛑"
-                case .tire:
-                    return "Tires 🛞"
-                case .suspension: 
-                    return "Suspension ⬇️"
-                case .eletric:
-                    return "Eletric ⚡️"
-                }
-            }
-        }
-    }
-    
-    struct Optionals: Decodable {
-        let eletricGlass: String
-        let airConditioned: String
-        let alarm: String
-        let powerSteering: String
-        
-        private enum CodingKeys: String, CodingKey {
-            case eletricGlass = "automaticGlass"
-            case airConditioned, alarm, powerSteering
-        }
-    }
-}
-
-// MARK: = Payload JSON
-let sampleJson = """
-[
-    {
-        "nick": "Lemonade",
-        "model": "Palio",
-        "manufacturer": "Fiat",
-        "plate": "mjmj123",
-        "production_year": "2004",
-        "acquisition_year": "2010",
-        "km": "234982",
-        "optionals" : {
-            "automaticGlass": "S",
-            "airConditioned": "N",
-            "alarm": "N",
-            "powerSteering": "S"
-        },
-        "maintenance_history": []
-    },
-    {
-        "nick": "Pikachu",
-        "model": "Fiesta",
-        "manufacturer": "Ford",
-        "plate": "mjhj321",
-        "production_year": "2000",
-        "acquisition_year": "2008",
-        "km": "234982",
-        "optionals" : {
-            "automaticGlass": "S",
-            "airConditioned": "S",
-            "alarm": "S",
-            "powerSteering": "S"
-        },
-        "maintenance_history": [
-            {
-                "type": 0,
-                "resume": "Oil change with filter",
-                "km": "235200",
-                "price": "230.00",
-            },
-            {
-                "type": 1,
-                "resume": "Change rear pads",
-                "km": "235800",
-                "price": "300.00",
-            },
-        ]
-    },
-]
-"""
-
-// MARK: - Decodificando o payload
-func decode() {
-    let decodeProduct: [Vehicle]
-    guard let data = sampleJson.data(using: .utf8) else {
-        return
-    }
-    let decoder = JSONDecoder()
-    decoder.keyDecodingStrategy = .convertFromSnakeCase
-    do {
-        decodeProduct = try decoder.decode([Vehicle].self, from: data)
-        printInformation(vehicle: decodeProduct)
-    } catch {
-        print("Ops!")
-    }
-}
-
-func printInformation(vehicle: [Vehicle]) {
-    vehicle.forEach { product in
-        print("""
-        Nick: \(product.nick)
-        Model: \(product.model)
-        Manufacturer: \(product.manufacturer)
-        Production year: \(product.productionYear)
-        Aquisition year: \(product.acquisitionYear)
-        Km: \(product.km)
-        Optionals: 
-         Glass: \(product.optionals.eletricGlass)
-         A.C.: \(product.optionals.airConditioned)
-         Alarm: \(product.optionals.alarm)
-         Power steering: \(product.optionals.powerSteering)\n
-        """)
-        if !product.services.isEmpty {
-            print("Maintenance history:")
-            product.services.forEach { maintenance in
-                print("""
-                 Type: \(maintenance.type.title)
-                 Resume: \(maintenance.resume)
-                 Km: \(maintenance.km)
-                 Price: \(maintenance.price)
-                \n
-                """)
-            }
-        }
-    }
-}
-//decode()
-
-
-// MARK: - Brincando com o init Decoder
-let imovelJSON = """
-{
-    "categoria": "casa",
-    "quantidade_quartos": 3,
-    "localizacao": {
-        "rua": "Edelmindo Silveira",
-        "numero": 12,
-        "cep": "88293875"
-    },
-    "observacoes": "FíDumaÉgua"
-}
-"""
-
-struct Imovel: Decodable {
+// MARK: - Estrutura de dados
+struct Imovel: Encodable {
     let categoria: String
     let quantidadeQuartos: Int
     let observacoes: String?
     let localizacao: Localizacao
     
-    struct Localizacao: Codable {
+    struct Localizacao: Encodable {
         let rua: String
         let numero: Int
         let cep: String
     }
 }
 
-func decodeImovel(data: Data) {
+// MARK: - Brincando com Encoder
+func encodeImovel(imovel: Imovel) {
+    let jsonEncoder = JSONEncoder()
+    jsonEncoder.keyEncodingStrategy = .convertToSnakeCase
+    
     do {
-        let decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
-        decoder.dateDecodingStrategy = .secondsSince1970
-        let decoded = try decoder.decode(Imovel.self, from: data)
-        printImovel(imovel: decoded)
+        let encodedData = try jsonEncoder.encode(imovel)
+        let encodedJSON = String(data: encodedData, encoding: .utf8) ?? ""
+        print(encodedJSON)
     } catch {
         print(error.localizedDescription)
     }
 }
 
-func printImovel(imovel: Imovel) {
-    print(imovel)
-}
+let imovel = Imovel(categoria: "casa",
+                    quantidadeQuartos: 3,
+                    observacoes: "Cômodos bem iluminados durante a manhã, pátio reservado com muros altos.",
+                    localizacao: Imovel.Localizacao(
+                        rua: "Edelmindo Silveira",
+                        numero: 12,
+                        cep: "88293875"))
 
-decodeImovel(data: Data(imovelJSON.utf8))
+encodeImovel(imovel: imovel)
